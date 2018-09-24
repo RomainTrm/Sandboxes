cbe9c4d411b7e35b9bceca202f4fa4c56d895444
Romain BERTHON <romain.berthon@ocea-smart-building.fr>
2018-09-24 14:16:49 +0200

    Functors

---
+++ FSharpSandbox/CategoryTherory.fsx
@@ -0,0 +1,47 @@
+// Note : Issue de https://medium.com/@lettier/your-easy-guide-to-monads-applicatives-functors-862048d61610
+
+open System
+
+module Functor =
+    let substract2 x = x - 2
+    let add1 = (+) 1
+    let mapList funtion list = List.map funtion list 
+
+    let ``A functor is a list`` () =
+        mapList add1 [1..3]
+        // Return [2; 3; 4]
+  
+    let ``A functor is a function`` () =
+        let mapFunction funtion1 funtion2 = funtion1 >> funtion2
+        let substract2_then_add1 = mapFunction substract2 add1
+        substract2_then_add1 1 
+        // Return 0
+
+    let ``A functor is a promise`` () =
+        let mapPromise funtion promise =
+            let return_promise () = 
+                async {
+                    let! promiseResult = promise ()
+                    return funtion promiseResult
+                }
+            return_promise
+        let promise () = async {
+                do! Async.Sleep 1
+                return 1 
+            }      
+        Async.RunSynchronously ((mapPromise add1 promise) ())
+        // Return 2
+
+    let ``Functor law 1 : Identity`` () =
+        let identity x = x
+        let list = [1..3]
+        list = mapList identity list
+        // Return true
+
+    let ``Functor law 2 : Composition`` () =
+        let multiply3 = (*) 3
+        let compositionFunction = multiply3 >> substract2 >> add1
+        // composition 1 = 2
+        let compositionList = mapList multiply3 >> mapList substract2 >> mapList add1
+        mapList compositionFunction [1..3] = compositionList [1..3]
+        // Return true
